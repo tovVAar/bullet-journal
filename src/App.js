3b705d8c2151f5bc4278b963c4f589ae3f65e91a
Erick Tovar <tovar.eri@gmail.com>
2018-03-19 19:36:19 -0600

    CRUD tutorial completo

--- src/App.js
+++ src/App.js
@@ -2,17 +2,68 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import MiComponente from './MiComponente';
+import ProductItem from './productItem.js'
+import AddProduct from './addProduct.js'
+
+const products = [
+  {
+    name: 'pepsi',
+    price: 10
+  },
+  {
+    name: 'coca',
+    price: 15
+  }
+];
+
+localStorage.setItem('products', JSON.stringify(products));
 
 class App extends Component {
+  componentWillMount(){
+    const myProducts = this.getProducts();
+    this.setState({ourProducts: myProducts});
+    console.log(myProducts);
+  }
+
+  getProducts(){
+    return this.state.ourProducts;
+  }
+
+
   constructor(props){
     super(props);
     this.onSubmit = this.onSubmit.bind(this);
 
     this.state = {
-      title: "Soy un título"
-    }
+      title: "Soy un título",
+      ourProducts: JSON.parse(localStorage.getItem('products'))
+    };
 
     this.enCambio = this.enCambio.bind(this);
+    this.onDelete = this.onDelete.bind(this);
+    this.onAdd = this.onAdd.bind(this);
+    this.onEditSubmit = this.onEditSubmit.bind(this)
+  }
+
+  onAdd(name, price){
+    const myProducts = this.getProducts();
+    myProducts.push({name, price});
+    console.log(name, price);
+    this.setState({
+      ourProducts: myProducts
+    });
+  }
+
+  onEditSubmit(name, price, originalName){
+    let myProducts = this.getProducts();
+    myProducts = myProducts.map(product => {
+      if(product.name === originalName){
+        product.name = name;
+        product.price = price;
+      }
+      return product;
+    });
+    this.setState({ourProducts: myProducts});
   }
 
   onClick(){
@@ -26,6 +77,16 @@ class App extends Component {
   onSubmit(event){
     event.preventDefault();
     console.log("Enviado " + this.input.value);
+  }
+
+  onDelete(name){
+    const myProducts = this.getProducts();
+
+    const filteredProducts = myProducts.filter(product => {
+      return product.name !== name;
+    });
+
+    this.setState({ourProducts: filteredProducts});
   }
 
   enCambio(){
@@ -49,6 +110,9 @@ class App extends Component {
         <p className="App-intro">
           Editale el <code>src/App.js</code> and save to reload.
         </p>
+        <AddProduct
+          onAdd = {this.onAdd}
+        />
         <div onClick={this.enCambio}>
           {this.state.title}
         </div>
@@ -74,6 +138,20 @@ class App extends Component {
             descripcion = "Es una señal."
             onClicked = {this.onClick}
           />
+        </div>
+        <div>
+          {
+            this.state.ourProducts.map( product => {
+              return (
+                <ProductItem
+                  key={product.name}
+                  {...product}
+                  onDelete = {this.onDelete}
+                  onEditSubmit = {this.onEditSubmit}
+                />
+              );
+            })
+          }
         </div>
       </div>
 
